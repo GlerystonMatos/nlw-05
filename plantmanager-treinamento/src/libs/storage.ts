5cb66c5e567c1af5572f3dfe17016a941728cf39
Gleryston Matos <glerystonmatos@yahoo.com.br>
2021-05-28 16:39:40 -0300

    Aula07.2

--- plantmanager-treinamento/src/libs/storage.ts
+++ plantmanager-treinamento/src/libs/storage.ts
@@ -18,6 +18,20 @@ export async function savePlant(plant: PlantProps): Promise<void> {
         const nextTime = new Date(plant.DateTimeNotification);
         const now = new Date();
 
+        const { Times, RepeatEvery } = plant.Frequency;
+
+        if (RepeatEvery === 'week') {
+            const interval = Math.trunc(7 / Times);
+            nextTime.setDate(now.getDate() + interval);
+        } else {
+            if (Times > 1)
+                nextTime.setDate(nextTime.getDate() + 1);
+        }
+
+        const seconds = Math.abs(
+            Math.ceil((now.getTime() - nextTime.getTime()) / 1000)
+        );
+
     } catch (error) {
         throw new Error(error);
     }
